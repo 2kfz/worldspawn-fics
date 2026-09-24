7282349d531d2bb756f933550814050803452b71
2kfz <78992902+2kfz@users.noreply.github.com>
2026-09-24 19:46:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const button = document.getElementById("nextButton");
+const pic1 = document.getElementById("pic1");
+const pic2 = document.getElementById("pic2");
 
+button.addEventListener("click", () => {
+    pic1.classList.add("leave");
+    pic2.classList.add("enter");
+});
